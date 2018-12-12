5c9e9995149660e11459b3758a321e738886ee13
Ronen Tamari <ronen.tamari1@mail.huji.ac.il>
2018-12-12 16:37:25 +0200

    update test

--- tests/test-make-lab-game-from-level.py
+++ tests/test-make-lab-game-from-level.py
@@ -17,7 +17,7 @@ from textworld.utils import make_temp_directory
 def test_game_walkthrough_agent(game_file):
         agent = textworld.agents.WalkthroughAgent()
         env = textworld.start(game_file)
-        env.activate_state_tracking()
+        env.enable_extra_info("score")
         commands = env.game.main_quest.commands
         agent.reset(env)
         game_state = env.reset()
@@ -28,14 +28,16 @@ def test_game_walkthrough_agent(game_file):
             command = agent.act(game_state, reward, done)
             game_state, reward, done = env.step(command)
         assert(done)
+        assert(reward == 1)
 
 
 if __name__ == "__main__":
     
-    test_seed = 32412
+    test_seed = 3241443
     level = 12
     lab_game_options = LabGameOptions()
     lab_game_options.seeds = test_seed
+    lab_game_options.force_recompile = True
     game = make_game_from_level(level, lab_game_options)
     
     quest = game.quests[0]
@@ -44,8 +46,8 @@ if __name__ == "__main__":
     pg.draw()
     
     with make_temp_directory(prefix="test_tw-make") as tmpdir:
-        output_folder = Path(tmpdir) / "gen_games"
+        output_folder = "gen_games"
         game_file = Path(output_folder) / ("%s.ulx" % (game.metadata["uuid"]))
-        game_file = textworld.generator.compile_game(game, str(game_file))
+        game_file = textworld.generator.compile_game(game, lab_game_options)
         # Solve the game using WalkthroughAgent.
         test_game_walkthrough_agent(game_file)
