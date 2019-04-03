2f409db896265ace6664d5e45322e74fe584be5a
Ronen Tamari <ronen.tamari1@mail.huji.ac.il>
2019-04-03 17:04:40 +0300

    Update documentation

--- textworld/challenges/lab_game.py
+++ textworld/challenges/lab_game.py
@@ -14,7 +14,7 @@ from textworld.challenges.utils import get_seeds_for_game_generation
 
 from textworld.utils import encode_seeds
 from textworld.generator.lab_game import LabGameOptions
-from textworld.generator.sketch_generator import SketchGenerationOptions, WinConditionType
+from textworld.generator.sketch_generator import SketchGenerationOptions, WinConditionType, init_usage_map
 from textworld.generator.process_graph import MATERIAL_STATES
 
 from textworld.utils import quest_gen_logger
@@ -38,50 +38,63 @@ def make_game_from_level(level: int, lab_game_options: Optional[LabGameOptions] 
     Notes:
         Difficulty levels are defined as follows:
 
-        * Level  1 to 10: mode easy, nb. devices and materials =  1,
-        start material state always powder. Max. quest length ranging
-          from 5 to 10 as the difficulty increases;
-        * Level 11 to 20: mode medium, nb. devices and materials 
-          between 2 and 3, start material states variable.
-          Quest length ranging from 10 to 15 as the difficulty 
-          increases;
-        * Level 21 to 30: mode hard, nb. devices 3 and nb. 
-          materials = 4 , quest length ranging from 15 to 22 as the difficulty
-          increases;
+        * Level  1 to 10: mode easy, nb. operations ranging from 1 to 2, 
+        nb. materials ranging from 2 to 3 as the difficulty increases.
+        Max number of descriptors per entity is 1;
+        * Level  11 to 20: mode medium, nb. operations ranging from 3 to 5, 
+        nb. materials ranging from 3 to 4 as the difficulty increases.
+        Max number of descriptors per entity is 2;
+        * Level  11 to 20: mode hard, nb. operations ranging from 4 to 6, 
+        nb. materials ranging from 5 to 8 as the difficulty increases.
+        Max number of descriptors per entity is 3;
 
-        where the different modes correspond to:
-
-        * Easy: Nb. materials/devices 2/1. Material start states 
-          always powder. Each device must be used exactly once.
-        * Medium: Nb. materials/devices 2-3/2-3 (variable). 
-          Material start states variable. Each device must be used 
-          at least 1-2 times, and at most 3 times.
-        * Hard: Nb. materials/devices 4/3 (variable). 
-          Material start states more variable than Medium level. Each
-          device must be used at least 1-3 times, and at most 3 times.
     """
     if not lab_game_options:
         lab_game_options = LabGameOptions()
 
+    lab_game_options.quest_reward = 1
+    
     if level >= 21:
         mode = "hard"
-        max_quest_lengths = np.round(np.linspace(15, 22, 10))
+        max_quest_lengths = np.round(np.linspace(60, 90, 10))
+        nb_materials_scale = np.round(np.linspace(4, 6, 10))
+        nb_ops_scale = np.round(np.linspace(5, 8, 10))
+        lab_game_options.preset_ops = True
         lab_game_options.max_quest_length = int(max_quest_lengths[level - 21])
+        lab_game_options.sketch_gen_options.nb_materials = int(nb_materials_scale[level - 21])
+        lab_game_options.sketch_gen_options.nb_ops = int(nb_ops_scale[level - 21])
+        lab_game_options.sketch_gen_options.max_descs_per_ent = init_usage_map(3)
     elif level >= 11:
         mode = "medium"
-        max_quest_lengths = np.round(np.linspace(10, 15, 10))
+        max_quest_lengths = np.round(np.linspace(50, 70, 10))
+        nb_materials_scale = np.round(np.linspace(3, 4, 10))
+        nb_ops_scale = np.round(np.linspace(3, 5, 10))
+        lab_game_options.preset_ops = True
         lab_game_options.max_quest_length = int(max_quest_lengths[level - 11])
+        lab_game_options.sketch_gen_options.nb_materials = int(nb_materials_scale[level - 11])
+        lab_game_options.sketch_gen_options.nb_ops = int(nb_ops_scale[level - 11])
+        lab_game_options.sketch_gen_options.max_descs_per_ent = init_usage_map(2)
     elif level >= 1:
         mode = "easy"
-        max_quest_lengths = np.round(np.linspace(5, 10, 10))
+        max_quest_lengths = np.round(np.linspace(20, 30, 10))
+        nb_materials_scale = np.round(np.linspace(2, 3, 10))
+        nb_ops_scale = np.round(np.linspace(1, 2, 10))
+        lab_game_options.preset_ops = True
         lab_game_options.max_quest_length = int(max_quest_lengths[level - 1])
+        lab_game_options.sketch_gen_options.nb_materials = int(nb_materials_scale[level - 1])
+        lab_game_options.sketch_gen_options.nb_ops = int(nb_ops_scale[level - 1])
+        lab_game_options.sketch_gen_options.max_descs_per_ent = init_usage_map(1)
+        
+    # no descriptors at all for level 1
+    if level == 1:
+        lab_game_options.sketch_gen_options.max_descs_per_ent = init_usage_map(0)
     
     modes = ["easy", "medium", "hard"]
     mode_idx = modes.index(mode)
     lab_game_options.surface_gen_options.set_difficulty_mode(mode)
     # Set max search steps according to difficulty if not otherwise specified
     if lab_game_options.default_sketch_opts:
-        lab_game_options.max_steps = ((mode_idx + 1) * 
+        lab_game_options.max_search_steps = ((mode_idx + 1) * 
     MAX_SEARCH_STEPS_PER_MODE)
         
 
@@ -93,28 +106,59 @@ def make_game(mode: str, lab_game_options: LabGameOptions
 
     Arguments:
         mode: Difficulty mode, where the different modes correspond to:
-
-        * Easy: Nb. materials/devices 2/1. Material start states 
-          always powder. Each device must be used exactly once.
-        * Medium: Nb. materials/devices 2-3/2-3 (variable). 
-          Material start states variable. Each device must be used 
-          at least 1-2 times, and at most 3 times.
-        * Hard: Nb. materials/devices 4/3 (variable). 
-          Material start states more variable than Medium level. Each
-          device must be used at least 1-3 times, and at most 3 times.
-
         lab_game_options: Options for customizing the game generation.
+
+    Returns:
+        game: Generated game.
+
+    Notes:
+        Difficulty modes are defined as follows:
+
+        *  easy: nb. operations ranging from 1 to 2, 
+        nb. materials ranging from 2 to 3 as the difficulty increases.
+        Max number of descriptors per entity is 1;
+        * medium: nb. operations ranging from 3 to 5, 
+        nb. materials ranging from 3 to 4 as the difficulty increases.
+        Max number of descriptors per entity is 2;
+        * hard: nb. operations ranging from 4 to 6, 
+        nb. materials ranging from 5 to 8 as the difficulty increases.
+        Max number of descriptors per entity is 3;
+
 
     Returns:
         Generated game.
     """
     
-    max_quest_length = lab_game_options.sketch_gen_options.max_depth
+    quest_gen_options = lab_game_options.sketch_gen_options
+    max_quest_length = quest_gen_options.max_depth
     surface_gen_options = lab_game_options.surface_gen_options
-    max_search_steps = lab_game_options.max_search_steps
+    quest_gen_options.win_condition = WinConditionType.ALL
+    
     # Deal with any missing random seeds.
     seeds = get_seeds_for_game_generation(lab_game_options.seeds)
 
+    
+
+    rng_objects = lab_game_options.rngs['objects']
+    rng_quest = lab_game_options.rngs['quest']
+    
+    n_materials = lab_game_options.sketch_gen_options.nb_materials
+    n_ops = lab_game_options.sketch_gen_options.nb_ops
+    
+    
+    all_op_types = KnowledgeBase.default().types.descendants('toe')
+    chosen_op_types = rng_objects.choice(all_op_types, n_ops, replace=True)
+    
+    if lab_game_options.preset_ops:
+        surface_gen_options.op_type_map = {'tlq_op_{}'.format(i): chosen_op_types[i] for i in range(n_ops)}
+    
+    sg = SurfaceGenerator(seed=seeds['surface'],
+                                         surface_gen_options=surface_gen_options)
+    quest_gen_options.quest_rng = rng_quest
+    
+    modes = ["easy", "medium", "hard"]
+    mode_idx = modes.index(mode)
+    
     metadata = {}  # Collect infos for reproducibility.
     metadata["orig_seed"] = lab_game_options.seeds
     metadata["desc"] = "Lab-Game"
@@ -124,45 +168,15 @@ def make_game(mode: str, lab_game_options: LabGameOptions
     metadata["max_quest_length"] = max_quest_length
     metadata["surface_gen_options"] = surface_gen_options.serialize()
 
-    rng_objects = np.random.RandomState(seeds['objects'])
-    rng_quest = np.random.RandomState(seeds['quest'])
     
-    quest_gen_logger.debug('Seeds are %s' % (str(seeds)))
-    sg = SurfaceGenerator(seed=seeds['surface'],
-                                         surface_gen_options=surface_gen_options)
-    
-    modes = ["easy", "medium", "hard"]
-    mode_idx = modes.index(mode)
-
-    # get all defined device types
-    device_types = uniquify(KnowledgeBase.default().types.descendants('sa'))
-    
+    # Currently unused
     if mode == "easy":
-        n_devices = 1
-        n_materials = 2
         material_states = ["powder"]
-        min_uses_per_device = {device_type: 1 for device_type \
-                               in device_types}
-        max_uses_per_device = {device_type: 1 for device_type \
-                               in device_types}
     elif mode == "medium":
-        n_devices =  rng_objects.choice([2, 3])
-        n_materials = rng_objects.choice([2, 3])
-        min_uses_per_device = {device_type: rng_objects.choice([1, 2]) for device_type \
-                               in device_types}
-        max_uses_per_device = {device_type: 3 for device_type \
-                               in device_types}
         material_states = rng_objects.choice(list(MATERIAL_STATES), n_materials, replace=True)
     elif mode == "hard":
-        n_devices = 3
-        n_materials = 4
         material_states = list(MATERIAL_STATES)
-        min_uses_per_device = {device_type: rng_objects.choice([1, 3]) for device_type \
-                               in device_types}
-        max_uses_per_device = {device_type: 3 for device_type \
-                               in device_types}
 
-    quest_gen_logger.debug("min_uses_per_device: %s" % (min_uses_per_device))
     M = LabGameMaker(surface_generator=sg)
 
     # Create the lab room.
@@ -171,44 +185,43 @@ def make_game(mode: str, lab_game_options: LabGameOptions
     M.set_player(lab)
     
     # - Describe the room.
-    lab.desc = "The lab is a magical place where you'll learn materials synthesis"
-    
-    # Used so that Inform7 will be able to reference names of any of the
-    # devices (needed for the single argument hack), but we only want 
-    # the player to be able to interact with the chosen ones.
-    limbo = M.new_room("Limbo")
-    limbo.desc = "Storeroom for unused devices."
-    
-    
+    lab.desc = "The lab is a magical place where you'll learn materials synthesis"    
 
     # Add materials to the world.
+    ent_type = 'm'
+    n_max_descs = quest_gen_options.max_descs_per_ent[ent_type]
     for i in range(n_materials):
         mat_state = rng_objects.choice(material_states)
         mat = M.new_lab_entity('m')
         mat.add_property(mat_state)
         lab.add(mat)
+        n_descs = rng_objects.randint(0, (n_max_descs + 1))
+        for j in range(n_descs):
+            mdesc = M.new_lab_entity('mdsc')
+            lab.add(mdesc)
+            quest_gen_options.ent_desc_map[mat.var.name].append(mdesc.var.name)
     
-    # Add lab_container to the world.
-    lab_container = M.new_lab_container()
-    lab.add(lab_container)
-        
-    # Add devices to the world.
-    chosen_devices = rng_objects.choice(device_types, min(n_devices, len(device_types)), replace=False)
-    for device_type in device_types:
-        device = M.new_lab_entity(device_type)
-        if device_type in chosen_devices:
-            lab.add(device)
-        else:
-            limbo.add(device)
+    # Add operations and their descriptors
+    ent_type = 'tlq_op'
+    n_max_descs = quest_gen_options.max_descs_per_ent[ent_type]
+    ops = []
     
-    # Set quest generation options.
-    quest_gen_options = SketchGenerationOptions(max_depth=max_quest_length,
-                            max_steps=max_search_steps,
-                            win_condition=WinConditionType.ALL,
-                            quest_rng=rng_quest,
-                            min_uses_per_device=min_uses_per_device,
-                            max_uses_per_device=max_uses_per_device
-                            )
+    # Either preset ops 
+    if lab_game_options.preset_ops:
+        ops = [M.new_tlq_op(dynamic_define=False, op_type=chosen_op_types[i]) for i in range(n_ops)]
+        op_type_map = {op.var.name: chosen_op_types[i] for i,op in enumerate(ops)}
+        assert op_type_map == surface_gen_options.op_type_map
+    else: # or make their classification part of the quest
+        ops = [M.new_tlq_op(dynamic_define=True) for i in range(n_ops)]
+    
+    for op in ops:
+        lab.add(op)
+        n_descs = rng_objects.randint(0, (n_max_descs + 1))
+        for j in range(n_descs):
+            odesc = M.new_lab_entity('odsc')
+            lab.add(odesc)
+            quest_gen_options.ent_desc_map[op.var.name].append(odesc.var.name)
+
     
     # Generate quest and corresponding surface (set in quest description)
     quest = M.generate_quest_surface_pair(quest_gen_options)
