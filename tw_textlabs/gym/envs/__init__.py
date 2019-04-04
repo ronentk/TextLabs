527803344301c4c0e2d645251a275462203c44d0
Ronen Tamari <ronen.tamari1@mail.huji.ac.il>
2019-04-04 12:34:10 +0300

    fix import

--- tw_textlabs/gym/envs/__init__.py
+++ tw_textlabs/gym/envs/__init__.py
@@ -1,2 +1,2 @@
 from tw_textlabs.gym.envs.batch_env import BatchEnv, ParallelBatchEnv
-from tw_textlabs.gym.envs.tw_textlabs_games_env import TextworldGamesEnv
+from tw_textlabs.gym.envs.textworld_games_env import TextworldGamesEnv
